aa2158056608d6c43bcb225b1508440e38375a36
Miguel S. Mendoza <miguel@smendoza.net>
2017-05-23 19:16:35 +0200

    Animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,85 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal <=> highlighted', animate(300))
+      //transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          backgroundColor: 'orange', 
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateY(-10px)'
+        }),
+        animate(300)]),
+      transition('* => void', [
+        animate(300, style({
+          opacity: 0,
+          transform: 'translateY(20px)'
+        }))])
+    ]),
+  ]
 })
 export class AppComponent {
-	list = ['Milk', 'Sugar', 'Bread'];
+  state = 'normal';
+  wildState = 'normal';
+  list = ['Milk', 'Sugar', 'Bread'];
 
-	onAdd(item) {
-		this.list.push(item);
-	}
+  onAnimate() {
+    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
 
-	onDelete(item) {
-		this.list.splice(this.list.indexOf(item), 1);
-	}
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
 }
